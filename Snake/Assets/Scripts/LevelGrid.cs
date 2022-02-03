6976d2ad37ab1630f4de83e8fdcd6ec2b384e639
Tathagat Chandra <tathagatchandra39@gmail.com>
2022-02-03 22:23:57 +0530

    Added Movement and food.
    Created a scene and added head of the snake with basic movement. Also added food in the scene which can be consumed and it respawns randomly within the map.

---
+++ Snake/Assets/Scripts/LevelGrid.cs
@@ -0,0 +1,46 @@
+// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+
+// public class LevelGrid //: MonoBehaviour
+// {
+//     private Vector2Int foodPosition;
+//     private GameObject foodGameObject;
+//     private int width;
+//     private int height;
+//     private Snake snake;
+
+//     public LevelGrid(int width, int height)
+//     {
+//         this.width = width;
+//         this.height = height;
+//     }
+
+//     public void SetUp(Snake snake)
+//     {
+//         this.snake = snake;
+
+//         SpawnFood();
+//     }
+
+//     private void SpawnFood()
+//     {
+//         do
+//         {
+//         foodPosition = new Vector2Int(Random.Range(0,width), Random.Range(0, height));
+//         }while (snake.GetGridPosition() == foodPosition);
+        
+//         foodGameObject = new GameObject("Food", typeof(SpriteRenderer));
+//         foodGameObject.GetComponent<SpriteRenderer>().sprite = GameAssets.instance.foodSprite;
+//         foodGameObject.transform.position = new Vector3(foodPosition.x, foodPosition.y);
+//     }
+
+//     public void SnakeMoved(Vector2Int snakeGridPosition)
+//     {
+//         if(snakeGridPosition == foodPosition)
+//         {
+//             Object.Destroy(foodGameObject);
+//             SpawnFood();
+//         }
+//     }
+// }
